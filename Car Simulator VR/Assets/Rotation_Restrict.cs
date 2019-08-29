59a3b3a9d6e6f01d25d33696f53fe4d262c300dd
sastaachar <justinjohnmathew100@gmai.com>
2019-08-29 17:51:07 +0530

    Add the rotation script
    Add the rotation script

---
+++ Car Simulator VR/Assets/Rotation_Restrict.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_Restrict : MonoBehaviour {
+
+    public float x, y, z;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(transform.eulerAngles.x + " - " + transform.eulerAngles.y + " - " + transform.eulerAngles.z);
+
+
+        if (transform.eulerAngles.y != y || transform.eulerAngles.z != z)
+        {
+            if (x > 271 && x < 280)
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, y, z);
+            else
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, z, y);
+        }
+
+        OVRGrabbable sn = gameObject.GetComponent<OVRGrabbable>();
+        if (!sn.isGrabbed)
+        {
+            transform.eulerAngles = new Vector3(x, 180, 180);
+        }
+        else
+        {
+            Debug.Log(sn.grabbedRigidbody );
+        }
+    }
+}
